e88966029efa8d6faab15add695a1355f747d375
ak-h-il <akhil18abc@gmail.com>
2025-07-05 23:55:03 +0530

    Add files via upload

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+        Welcome Home! Anywhere you Go <br /> Enjoy the moment. Make your
+        memories
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
